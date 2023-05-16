595cdba7a426d4de8c6b336a575d750d27d75f01
Jeremy Johns <jez.johns@gmail.com>
2023-05-16 13:41:54 +0000

    added app.post

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const mongoose = require("mongoose");
 const PORT = 8080; //process.env.PORT ||
 require("dotenv").config();
 
+const app = express();
+app.use(cors());
+const bp = require("body-parser");
+app.use(bp.json());
 const Book = require("./models/book");
 
 mongoose.connect(process.env.MONGO_URL);
-
-const app = express();
-app.use(cors());
 
 app.get("/", (request, response) => {
 	response.json("You have reached the root. How may I help?");
@@ -18,6 +19,15 @@ app.get("/", (request, response) => {
 app.get("/books", async (request, response) => {
 	const books = await Book.find();
 	response.json(books);
+});
+
+app.post("/books", async (request, response) => {
+	const newBook = await Book.create(request.body);
+	// {title: "1984",
+	// description: "Dystopia",
+	// status: "In print",}
+
+	console.log("Book posted");
 });
 
 app.listen(PORT, () => console.log("App  listening on PAWT" + PORT));
